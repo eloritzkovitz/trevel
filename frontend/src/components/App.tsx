d05c5cb60815c8134b30ae3bbf5c716a69a1306b
Elor-Itz <elor2200@gmail.com>
2025-03-01 11:32:31 +0200

    Added components for login and signup

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
+import Login from "./Login";
+import Signup from "./Signup";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainPage />} />        
+        <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />        
       </Routes>
     </Router>
   );
