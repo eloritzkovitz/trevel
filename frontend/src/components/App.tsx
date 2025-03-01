a9afbfe1395191858d2b7f35878b12a6e9c6074a
Elor-Itz <elor2200@gmail.com>
2025-03-01 21:21:04 +0200

    Added profile page

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
 import Login from "./Login";
 import Signup from "./Signup";
+import Profile from "./Profile";
 
 const App: React.FC = () => {
   return (
@@ -10,7 +11,8 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />        
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/profile" element={<Profile />} />        
       </Routes>
     </Router>
   );
