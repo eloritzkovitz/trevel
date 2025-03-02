6397ca8d01bd4ad2fa34551fe0ef63dee7ba002e
Elor-Itz <elor2200@gmail.com>
2025-03-02 16:18:12 +0200

    Added navbar component

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,44 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 import PostsList from "./PostsList";
 
 const Signup: React.FC = () => {
   return (
     <div className="container-fluid bg-light min-vh-100">
       {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container">
-          <a className="navbar-brand" href="/">
-            Trevel
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/profile">
-                  Profile
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link btn btn-danger text-white" href="#">
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar/>
 
       {/* Main Content */}
       <div className="container mt-4">
