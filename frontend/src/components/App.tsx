f2ec9da2e652630b4fbc5583d79da111dae45174
Elor-Itz <elor2200@gmail.com>
2025-03-06 17:57:06 +0200

    Added GoogleOAuthProvider

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -5,19 +5,22 @@ import Login from "./Login";
 import Signup from "./Signup";
 import Profile from "./Profile";
 import { AuthProvider, useAuth } from "../context/AuthContext";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const App: React.FC = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/" element={<RequireAuth><MainPage /></RequireAuth>} />
-          <Route path="/profile/:userId" element={<RequireAuth><Profile /></RequireAuth>} />       
-        </Routes>
-      </Router>
-    </AuthProvider>    
+    <GoogleOAuthProvider clientId={import.meta.env.REACT_APP_GOOGLE_CLIENT_ID || ""}>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<RequireAuth><MainPage /></RequireAuth>} />
+            <Route path="/profile/:userId" element={<RequireAuth><Profile /></RequireAuth>} />       
+          </Routes>
+        </Router>
+      </AuthProvider> 
+    </GoogleOAuthProvider>       
   );
 };
 
