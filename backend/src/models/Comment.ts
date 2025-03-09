6f530a3cc290d7f365e06ec71e431e91eff12e24
Elor-Itz <elor2200@gmail.com>
2025-03-09 14:59:04 +0200

    Updated models

--- backend/src/models/Comment.ts
+++ backend/src/models/Comment.ts
@@ -1,12 +1,20 @@
 import mongoose from "mongoose";
 
 export interface IComment {
+  sender: String;
   postId: string;
   content: string;
-  sender: String;  
+  likes: mongoose.Schema.Types.ObjectId[];
+  likesCount: number;
+  createdAt: string;
+  updatedAt: string;   
 }
 
 const commentSchema = new mongoose.Schema<IComment>({
+  sender: {
+    type: String,
+    required: true,
+  },
   postId: {
     type: String,
     required: true 
@@ -15,10 +23,21 @@ const commentSchema = new mongoose.Schema<IComment>({
     type: String, 
     required: true 
   },
-  sender: {
-      type: String,
-      required: true,
-    },  
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: [],    
+  }],
+  likesCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: String,
+  },
+  updatedAt: {
+    type: String,
+  },    
 });
 
 const commentsModel = mongoose.model<IComment>("Comments", commentSchema);
--- backend/src/models/Post.ts
+++ backend/src/models/Post.ts
@@ -1,22 +1,49 @@
 import mongoose from "mongoose";
 
 export interface IPost {
+  sender: mongoose.Schema.Types.ObjectId;
   title: string;
   content: string;
-  sender: String;  
+  images?: string[];
+  likes: mongoose.Schema.Types.ObjectId[];
+  likesCount: number;
+  comments: mongoose.Schema.Types.ObjectId[];
+  createdAt: string; 
+  updatedAt: string; 
 }
 
 const postSchema = new mongoose.Schema<IPost>({
-  title: {
-    type: String,
-    required: true,
-  },
-  content: String,
   sender: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User', 
     required: true,
   },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: String,
+  images: [String],
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    default: [],
+  }],
+  likesCount: {
+    type: Number,
+    default: 0,
+  },
+  comments: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment',
+    default: [],
+  }], 
+  createdAt: {
+    type: String,
+  },
+  updatedAt: {
+    type: String,    
+  }, 
 });
 
 const postModel = mongoose.model<IPost>("Posts", postSchema);
