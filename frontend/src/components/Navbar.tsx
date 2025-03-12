0e6f375f82864b3ca2d1eb4fbb6d460d2e4625c8
Elor-Itz <elor2200@gmail.com>
2025-03-12 14:44:13 +0200

    Redesigned navbar and app icon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,54 +1,125 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container, Form, FormControl, InputGroup} from "react-bootstrap";
 import logo from "../assets/logo.png";
 import { useAuth } from "../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faHome, faPlane, faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 const NavigationBar: React.FC = () => {
   const { user, logout } = useAuth();
 
   return (
-    <Navbar bg="primary" variant="dark" expand="lg" fixed="top">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img src={logo} alt="Trevel Logo" style={{ height: '50px', marginRight: '10px' }} />
-          Trevel
+    <Navbar
+      bg="white"
+      variant="light"
+      expand="lg"
+      fixed="top"
+      style={{ boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.1)" }}
+    >
+      <Container fluid>
+        {/* Brand */}
+        <Navbar.Brand as={Link} to="/" className="ms-3">
+          <img src={logo} alt="Trevel Logo" style={{ height: "50px" }} />
         </Navbar.Brand>
+
+        {/* Search */}
+        <Form
+          className="d-flex ms-auto position-relative"
+          style={{ height: "60px", width: "300px" }}
+        >
+          <InputGroup>
+            <FontAwesomeIcon
+              icon={faSearch}
+              className="position-absolute text-muted"
+              style={{
+                height: "20px",
+                left: "15px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                zIndex: 1,
+              }}
+            />
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2 rounded-pill ps-5"
+              aria-label="Search"
+              style={{ fontSize: "24px", backgroundColor: "#f8f9fa", borderColor: "#f8f9fa", boxShadow: "inset 1px 1px 1px rgba(0, 0, 0, 0.03)",
+              }}
+            />
+          </InputGroup>
+        </Form>
+
+        {/* Menu */}
         <Navbar.Toggle aria-controls="navbarNav" />
-        <Navbar.Collapse id="navbarNav">
-          <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">
-              <FontAwesomeIcon icon={faHome} style={{ marginRight: '5px' }} />
-              Home
+        <Navbar.Collapse
+          id="navbarNav"
+          className="d-flex justify-content-between w-100">         
+          <Nav className="mx-auto">
+            <Nav.Link
+              as={Link}
+              to="/"
+              className={`nav-link d-flex flex-column align-items-center ${location.pathname === "/" ? "active text-primary" : ""}`}
+              style={{ marginRight: "15px" }}
+            >
+              <FontAwesomeIcon icon={faHome} style={{ height: "25px" }} />
+              <span>Home</span>
             </Nav.Link>
-            {user && (
-              <NavDropdown
-                title={
-                  <>
-                    <img 
-                      src={user.profilePicture} 
-                      alt="Profile" 
-                      style={{ width: '30px', height: '30px', borderRadius: '50%', marginRight: '10px' }} 
-                    />
-                    {user.firstName} {user.lastName}
-                  </>
-                }
-                id="navbarDropdown"
-                align="end"
-              >
-                <NavDropdown.Item as={Link} to={`/profile/${user._id}`}>
-                  <FontAwesomeIcon icon={faUser} style={{ marginRight: '5px' }} />
-                  Profile
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={logout}>
-                  <FontAwesomeIcon icon={faSignOutAlt} style={{ marginRight: '5px' }} />
-                  Logout
-                </NavDropdown.Item>
-              </NavDropdown>
-            )}
+
+            <Nav.Link
+              as={Link}
+              to="/trips"
+              className={`nav-link d-flex flex-column align-items-center ${location.pathname === "/trips" ? "active text-primary" : ""}`}
+              style={{ marginRight: "15px" }}
+            >
+              <FontAwesomeIcon icon={faPlane} style={{ height: "25px" }} />
+              <span>Trips</span>
+            </Nav.Link>
           </Nav>
+
+          {/* Profile Dropdown */}
+          {user && (
+            <NavDropdown
+              title={
+                <>
+                  <img
+                    src={user.profilePicture}
+                    alt="Profile"
+                    style={{ height: "50px", width: "50px",  borderRadius: "50%" }}
+                  />
+                </>
+              }
+              id="navbarDropdown"
+              align="end"
+              className="ms-5"
+            >
+              <NavDropdown.Item>
+                <>
+                  <img
+                    src={user.profilePicture}
+                    alt="Profile"
+                    style={{ height: "50px", width: "50px",  borderRadius: "50%" }}
+                  />
+                </>
+                <span className="fw-semibold">
+                  {user.firstName} {user.lastName}
+                </span>
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to={`/profile/${user._id}`}>
+                <FontAwesomeIcon icon={faUser} style={{ marginRight: "5px" }} />
+                Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item onClick={logout}>
+                <FontAwesomeIcon
+                  icon={faSignOutAlt}
+                  style={{ marginRight: "5px" }}
+                />
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
